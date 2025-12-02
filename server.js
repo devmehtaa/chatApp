c0e835dbf8ec678b83c4223cd8fba163104cbb2a
Dev Mehta <139632302+devmehtaa@users.noreply.github.com>
2025-12-02 02:31:27 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const http = require('http')
+const {Server} = require('socket.io')
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    }
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let chatHistory = [];
+
+io.on("connection", (socket) => {
+    console.log("User Connected");
+    socket.emit("history", chatHistory);
+
+    socket.on("message", (text) => {
+        console.log(`Message sent: ${text.text}`);
+        chatHistory.push(text);
+        if(chatHistory.length > 100){
+            chatHistory.shift();
+        }
+        socket.broadcast.emit("message", text);
+    });
+});
+
+
+server.listen(8080, () =>{
+    console.log("http://localhost:8080/");
+});
